8cec14a39219a6fbed9e23de68040bcfbb9c65a4
Ibrahim Ali <112340231+IbrahimXYZ@users.noreply.github.com>
2023-03-24 02:42:39 -0700

    Update nutrition.js

--- JS/nutrition.js
+++ JS/nutrition.js
@@ -1,47 +1,32 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '238795571cmshd0b89581c6f3b5ap1e6bc6jsn1d71c893b31d',
+		'X-RapidAPI-Key': '802783ea7dmshc95c0811543dd5cp19f785jsnd5006bd571bb',
 		'X-RapidAPI-Host': 'nutrition-by-api-ninjas.p.rapidapi.com'
 	}
 };
 
-/*fetch('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=apple', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));*/
+const button = document.getElementById('nutri-btn');
+const searchInput = document.getElementById('nutri-srch');
+const container = document.querySelector('.nutritionContainer__Information');
 
-/*function getCalories(food){
-	fetch('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=orange', options)
+
+
+
+
+
+button.addEventListener('click', ()=>{
+	fetch('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=' + searchInput.value, options)
 	.then(response => response.json())
-	.then(data =>{
-		const list =data;
-		list.map((item) =>{
+	.then(data => {
+		container.innerHTML = ""
+		const list = data;
+		list.map((item)=>{
 			const name = item.name;
-			const calories =item.calories;
+			const calories = item.calories;
 			const servingSize = item.serving_size_g;
-			const totalFat = item.fat_total_g;
-			const saturatedFat = item.fat_saturated_g;
-			const protein = item.protein_g
-		});
-	})
-	.catch(err => console.error(err));
-}*/
-
-
-
-
-function getNutrition(food){
-	fetch(('https://nutrition-by-api-ninjas.p.rapidapi.com/v1/nutrition?query=' + food),options)
-	.then(response => response.json())
-	.then(data =>{
-		const list =data;
-		list.map((item) =>{
-			const name = item.name;
-			const calories =item.calories;
-			const servingSize = item.serving_size_g;
-			const totalFat = item.fat_total_g;
-			const saturatedFat = item.fat_saturated_g;
+			const fatTotal = item.fat_total_g;
+			const fatSaturated = item.fat_saturated_g;
 			const protein = item.protein_g;
 			const sodium = item.sodium_mg;
 			const potassium = item.potassium_mg;
@@ -49,26 +34,23 @@ function getNutrition(food){
 			const carbohydrates = item.carbohydrates_total_g;
 			const fiber = item.fiber_g;
 			const sugar = item.sugar_g;
-			const card = `<h1>${name}</h1>
-						<ul>
-							<li>calories : ${calories}</li>
+			const label = `
+							<ul>
+							<li>${name}</li>
+							<li>Calories : ${calories}</li>
 							<li>Serving Size (g) : ${servingSize}</li>
-							<li>Total Fat (g) : ${totalFat}</li>
-							<li>Saturated Fat (g) : ${saturatedFat}</li>
+							<li>Total Fat (g) : ${fatTotal}</li>
+							<li>Saturated Fat (g) ${fatSaturated}</li>
 							<li>Protein (g) : ${protein}</li>
 							<li>Sodium (mg) : ${sodium}</li>
 							<li>Potassium (mg) : ${potassium}</li>
 							<li>Cholestrol (mg) : ${cholestrol}</li>
-							<li>Total Carbohydrates : ${carbohydrates}</li>
+							<li>Carbohydrates (mg) : ${carbohydrates}</li>
 							<li>Fiber (g) : ${fiber}</li>
 							<li>Sugar (g) : ${sugar}</li>
-							</ul>`
+							</ul> `
 
-			document.querySelector('.nutritionContainer__Information').innerHTML += card
-							
+			container.innerHTML += label})
 })})
-}
 
-const search = document.getElementById('nutri-btn');
-const searchInput = document.getElementById('nutri-srch').value;
-search.addEventListener('click',getNutrition(searchInput));
+
